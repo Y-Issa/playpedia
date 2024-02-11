b29fa6a3094c87e231cf92179df72d08d4838fa8
Youssef Issa <youssefissa232@gmail.com>
2024-02-11 22:45:39 +0200

    Details Box

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import StarRating from "./StarRating";
 const KEY = "34ada40ed97349ef91ac21d163906a28";
 
 export default function App() {
@@ -7,10 +7,10 @@ export default function App() {
   const [query, setQuery] = useState("");
   const [games, setGames] = useState([]);
   const [selectedId, setSelectedId] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleShowDetails(id) {
-    setSelectedId(id);
-    console.log(id);
+    setSelectedId(selectedId !== id ? id : null);
   }
 
   useEffect(
@@ -19,6 +19,7 @@ export default function App() {
 
       async function fetchData() {
         try {
+          setIsLoading(true);
           const res = await fetch(
             `https://api.rawg.io/api/games?key=${KEY}&search=${query}`,
             { signal: controller.signal }
@@ -34,6 +35,8 @@ export default function App() {
           setGames(data.results);
         } catch (err) {
           console.log(err.message);
+        } finally {
+          setIsLoading(false);
         }
       }
       if (query.length > 2) fetchData();
@@ -52,7 +55,11 @@ export default function App() {
       </Navbar>
       <Main>
         <Box>
-          <GamesList games={games} onShowDetails={handleShowDetails} />
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <GamesList games={games} onShowDetails={handleShowDetails} />
+          )}
         </Box>
         <Box>{selectedId && <Details selectedId={selectedId} />}</Box>
       </Main>
@@ -81,6 +88,7 @@ function Search({ query, setQuery }) {
   return (
     <input
       type="text"
+      className="search"
       placeholder="Search for a game..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
@@ -90,6 +98,14 @@ function Search({ query, setQuery }) {
 
 function Main({ children }) {
   return <main className="main">{children}</main>;
+}
+
+function Loader() {
+  return (
+    <div className="loader">
+      <p>LOADING...</p>
+    </div>
+  );
 }
 
 function Box({ children }) {
@@ -128,5 +144,63 @@ function Game({ game, onShowDetails }) {
 }
 
 function Details({ selectedId }) {
-  return <div>{selectedId}</div>;
+  const [game, setGame] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(
+    function () {
+      async function fetchMovieDetails() {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.rawg.io/api/games/${selectedId}?key=${KEY}`
+        );
+        const data = await res.json();
+        console.log(data);
+        setGame(data);
+        setIsLoading(false);
+      }
+      fetchMovieDetails();
+    },
+    [selectedId]
+  );
+  return (
+    <div className="details">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <header>
+            <img src={game.background_image} alt={game.slug} />
+            <div className="details-overview">
+              <h3>{game.name}</h3>
+              <p>{new Date(game.released).toDateString()}</p>
+              <p>
+                Platforms:{" "}
+                {game.platforms?.map((platform, i) => (
+                  <span key={i}>{platform.platform.name} </span>
+                ))}
+              </p>
+              {game.publishers?.length > 0 ? (
+                <p>
+                  Published by:{" "}
+                  {game.publishers?.map((publisher, i) => (
+                    <span>
+                      {publisher.name}
+                      {i < game.publishers.length - 1 && ", "}
+                    </span>
+                  ))}
+                </p>
+              ) : (
+                ""
+              )}
+            </div>
+          </header>
+          <StarRating />
+          <div
+            className="description"
+            dangerouslySetInnerHTML={{ __html: game.description }}
+          />
+        </>
+      )}
+    </div>
+  );
 }
